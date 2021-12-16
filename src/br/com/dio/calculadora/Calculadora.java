9366f86b79c43bbf2ed5c76226618e63ae1316af
Alaor Castelano <alaor.softengineer@gmail.com>
2021-12-15 22:37:18 -0300

    Calculadora simples.

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,49 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        float a;
+        float b;
+
+        System.out.println("Digite o primeiro valor:");
+            a = scan.nextInt();
+
+        System.out.println("Digite o segundo valor:");
+            b = scan.nextInt();
+
+        float somar = somar(a, b);
+        float subtrair = subtrair(a, b);
+        float multiplicar = multiplicar(a, b);
+        float dividir = dividir(a, b);
+
+        System.out.println("Somar: " + somar);
+        System.out.println("Subtrair: " + subtrair);
+        System.out.println("Multiplicar: " + multiplicar);
+
+
+    }
+
+    public static float somar(float a, float b) {
+        return a + b;
+    }
+
+    public static float subtrair(float a, float b) {
+        return a - b;
+    }
+
+    public static float multiplicar(float a, float b) {
+        return a * b;
+    }
+
+    public static float dividir(float a, float b) {
+        return a / b;
+    }
+
+
+}
